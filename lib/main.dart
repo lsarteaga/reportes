6c61d6b4f1b92dc3d8cfe93eddb156abf7b2d584
lsarteaga <lsarteaga_dev@protonmail.ch>
2021-03-03 11:53:44 -0500

    firebase sdk agregado

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reportes/Pages/home_page.dart';
 import 'package:reportes/Providers/content_provider.dart';
@@ -22,6 +23,8 @@ class MyApp extends StatelessWidget {
   final prefs = new Preferences();
   @override
   Widget build(BuildContext context) {
+    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
     return ChangeNotifierProvider<ContentProvider>(
       create: (BuildContext context) => ContentProvider(),
       child: Consumer<ContentProvider>(
@@ -30,7 +33,26 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
             title: 'Project 3738',
             debugShowCheckedModeBanner: false,
-            home: HomePage(),
+            //home: HomePage(),
+            home: FutureBuilder(
+              future: _initialization,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Container(
+                    child: Center(
+                      child: Text('Error de inicio',
+                          style: Theme.of(context).textTheme.headline3),
+                    ),
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.done) {
+                  print('Firebase iniciado correctamente');
+                  return HomePage();
+                }
+                return Container(
+                    child: Center(child: CircularProgressIndicator()));
+              },
+            ),
             theme: MainTheme.setup(provider.darkMode),
           );
         },
